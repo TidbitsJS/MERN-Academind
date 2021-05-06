17c681a8793a65301bae06f5840c6ec811d19eee
Tidbits JS <tidbitsjs@gmail.com>
2021-05-07 01:01:37 +0530

    backend api

---
+++ BasicApp/backend/server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const uuid = require("uuid");
+
+const app = express();
+
+const DUMMY_PRODUCTS = [];
+const PORT = 5000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accpet, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+
+  next();
+});
+
+app.get("/products", (req, res, next) => {
+  res.status(200).json({ products: DUMMY_PRODUCTS });
+});
+
+app.post("/product", (req, res, next) => {
+  const { title, price } = req.body;
+
+  if (!title || title.trim().length === 0 || !price || price <= 0) {
+    return res.status(422).json({
+      message: "Invalid input, please enter a valid title and price",
+    });
+  }
+
+  const createdProduct = {
+    id: uuid(),
+    title,
+    price,
+  };
+
+  DUMMY_PRODUCTS.push(createdProduct);
+  res
+    .status(201)
+    .json({ message: "Created new product.", product: createdProduct });
+});
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
