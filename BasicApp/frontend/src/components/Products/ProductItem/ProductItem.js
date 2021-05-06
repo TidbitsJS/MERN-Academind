46eedc768132587b65e71a47f8825f95274d4b4b
Tidbits JS <tidbitsjs@gmail.com>
2021-05-07 00:48:26 +0530

    product item & list component

---
+++ BasicApp/frontend/src/components/Products/ProductItem/ProductItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ProductItem = (props) => {
+  return (
+    <li className="product-item">
+      <h2>{props.name}</h2>
+      <p>Price: ${props.price}</p>
+    </li>
+  );
+};
+
+export default ProductItem;
