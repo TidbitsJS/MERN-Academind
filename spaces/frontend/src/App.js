1de91bc7b26e79b52072db8e398a54f0b905156d
Tidbits JS <tidbitsjs@gmail.com>
2021-05-11 22:49:26 +0530

    frontend project setup

---
+++ spaces/frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
