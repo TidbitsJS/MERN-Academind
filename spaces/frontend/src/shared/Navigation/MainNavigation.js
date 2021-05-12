ece383624721e535461bc110b024cef2b4ae10ae
Tidbits JS <tidbitsjs@gmail.com>
2021-05-12 19:32:04 +0530

    drawer customization

--- spaces/frontend/src/shared/Navigation/MainNavigation.js
+++ spaces/frontend/src/shared/Navigation/MainNavigation.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import NavLinks from "./NavLinks";
 import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 import "./mainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawer = () => setDrawerIsOpen(true);
+  const closeDrawer = () => setDrawerIsOpen(false);
+
   return (
     <>
-      <SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawer} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawer}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
           <span />
           <span />
           <span />
