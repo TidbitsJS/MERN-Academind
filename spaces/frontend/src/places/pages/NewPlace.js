d6143733389675952e0a716ee0d0ae2411ab2866
Tidbits JS <tidbitsjs@gmail.com>
2021-05-16 19:18:50 +0530

    custom form input

--- spaces/frontend/src/places/pages/NewPlace.js
+++ spaces/frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import Input from "../../shared/FormElements/Input";
+import "./newPlace.css";
+
 const NewPlace = () => {
-  return <h2>NewPlace Works!</h2>;
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText={"Please enter a valid title"}
+      />
+    </form>
+  );
 };
 
 export default NewPlace;
