b0bcefc5d85e0367ef4106081a0b9efe4504586c
Tidbits JS <tidbitsjs@gmail.com>
2021-05-20 11:35:14 +0530

    GET place routes & error handling

--- placere/backend/server.js
+++ placere/backend/server.js
@@ -7,6 +7,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(placesRoutes);
+app.use("/api/places", placesRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "Unknow error occured!" });
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
