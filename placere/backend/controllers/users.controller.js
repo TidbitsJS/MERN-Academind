ad45482ba65698e1289bb972dc0b0eace40a3625
Tidbits JS <tidbitsjs@gmail.com>
2021-05-27 19:14:27 +0530

    login & get users route

--- placere/backend/controllers/users.controller.js
+++ placere/backend/controllers/users.controller.js
@@ -11,8 +11,19 @@ const DUMMY_USERS = [
   },
 ];
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let allUsers;
+  try {
+    allUsers = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Fetching users failed, please try again later.",
+      500
+    );
+    return next(error);
+  }
+
+  res.json({ users: allUsers.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
@@ -62,7 +73,7 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return next(
@@ -72,7 +83,14 @@ const login = (req, res, next) => {
 
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  let identifiedUser;
+  try {
+    identifiedUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("Logging up failed, please try again", 500);
+    return next(error);
+  }
+
   if (!identifiedUser || identifiedUser.password !== password) {
     const error = new HttpError(
       "Could not identify user, credentials seem to be wrong",
