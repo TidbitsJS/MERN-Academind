8bdc279fa7592c2a039067fc40a658c105d3e31f
Tidbits JS <tidbitsjs@gmail.com>
2021-06-01 22:16:10 +0530

    uploading files with multer

--- placere/backend/routes/users.routes.js
+++ placere/backend/routes/users.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users.controller");
+const fileUpload = require("../middleware/fileUpload");
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single("image"),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
--- placere/frontend/src/user/pages/Auth.js
+++ placere/frontend/src/user/pages/Auth.js
@@ -84,17 +84,16 @@ const Auth = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
+
         const responseData = await sendRequest(
           `${process.env.REACT_APP_SERVER_URI}/api/users/signup`,
           "POST",
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            "Content-Type": "application/json",
-          }
+          formData
         );
 
         auth.login(responseData.user.id);
