e9ebfc6a5a575b8199475f697590ee9f617daf96
Tidbits JS <tidbitsjs@gmail.com>
2021-07-13 17:06:49 +0530

    create models & some items

---
+++ locallibrary/models/genre.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema({
+  name: { type: String, minLength: 3, maxLength: 100, required: true },
+});
+
+// Virtual for this genre instance URL
+GenreSchema.virtual("url").get(() => {
+  return "/catalog/genre" + this._id;
+});
+
+// Export model
+module.exports = mongoose.model("Genre", GenreSchema);
