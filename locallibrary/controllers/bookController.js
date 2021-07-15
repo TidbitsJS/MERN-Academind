6092892a7b68e3ae01185f4073995fb2e3c9763e
Tidbits JS <tidbitsjs@gmail.com>
2021-07-15 16:54:40 +0530

    home page

--- locallibrary/controllers/bookController.js
+++ locallibrary/controllers/bookController.js
@@ -1,7 +1,37 @@
 const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
+
+const async = require("async");
 
 exports.index = (req, res) => {
-  res.send("NOT IMPLEMENTED: Site Home Page");
+  async.parallel(
+    {
+      book_count: (cb) => {
+        Book.countDocuments({}, cb);
+      },
+      book_instance_count: (cb) => {
+        BookInstance.countDocuments({}, cb);
+      },
+      book_instance_available_count: (cb) => {
+        BookInstance.countDocuments({ status: "Available" }, cb);
+      },
+      author_count: (cb) => {
+        Author.countDocuments({}, cb);
+      },
+      genre_count: (cb) => {
+        Genre.countDocuments({}, cb);
+      },
+    },
+    (err, results) => {
+      res.render("index", {
+        title: "Local Library Home",
+        error: err,
+        data: results,
+      });
+    }
+  );
 };
 
 // Display list of all books
