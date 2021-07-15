d1293ec985c77b908fbd281f0ad1b5d45adcf344
Tidbits JS <tidbitsjs@gmail.com>
2021-07-15 17:24:56 +0530

    bookInstance list page

--- locallibrary/controllers/bookinstanceController.js
+++ locallibrary/controllers/bookinstanceController.js
@@ -1,8 +1,19 @@
 const BookInstance = require("../models/bookinstance");
 
 // Display list of all BookInstances
-exports.bookinstance_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: BookInstance list");
+exports.bookinstance_list = (req, res, next) => {
+  BookInstance.find()
+    .populate("book")
+    .exec((err, list_bookinstances) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.render("bookinstance_list", {
+        title: "Book Instance List",
+        bookinstance_list: list_bookinstances,
+      });
+    });
 };
 
 // Display detail page for a specific BookInstance
--- locallibrary/models/bookinstance.js
+++ locallibrary/models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -16,7 +17,12 @@ const BookInstanceSchema = new Schema({
 
 // Virtual for bookinstance's URL
 BookInstanceSchema.virtual("url").get(function () {
-  return "/catalog/bookinstance" + this._id;
+  return "/catalog/bookinstance/" + this._id;
+});
+
+// Virtual for bookinstance's date formatting
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
